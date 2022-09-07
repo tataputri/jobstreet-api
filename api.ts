05b1ac2f9bbf18d50f85c2607306e8c0c08ae9b1
tata putri <105484949+tataputri@users.noreply.github.com>
2022-09-07 21:31:50 +0700

    add api integration to user management

---
+++ api.ts
@@ -0,0 +1,50 @@
+import { Request, Response, NextFunction } from 'express'
+import axios from "axios"
+
+
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+
+  if (!req.headers.apikey) {
+    return res.send({
+      success: false,
+      error: 'please provide api key'
+    })
+  }
+  const ApiEndPoint = `${process.env.API_BASE}/api/collections/jobstreet_id_api/records?filter=(apikey='${req.headers.apikey}')`
+
+  const ApiStatus = await axios.get(ApiEndPoint).then(res => res.data)
+  if (ApiStatus.items.length < 1) {
+    return res.send({
+      success: false,
+      error: 'Please provide a valid Api Key'
+    })
+  }
+
+  const apiKeyStatus = ApiStatus.items[0];
+
+  const today = new Date();
+  const expiredDate = new Date(apiKeyStatus.expired);
+  console.log(`Expired Date : ${expiredDate}`)
+  console.log(`Today Date : ${today}`)
+
+
+  if (apiKeyStatus.status === false) {
+    return res.send({
+      success: false,
+      error: 'Api key is disabled. please contact admin at discord @nulled#7662'
+    })
+  }
+  if (expiredDate <= today) {
+    return res.send({
+      success: false,
+      error: 'Api key expired. please contact admin at discord @nulled#7662'
+    })
+  }
+
+  // if (req.authenticated) next()
+  // console.log(req.headers)
+
+  next()
+
+}
--- app.ts
+++ app.ts
@@ -1,12 +1,16 @@
 import express from "express"
-import createRouter,{router} from "express-file-routing"
+import createRouter, { router } from "express-file-routing"
+import checkApi from './api'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 const app = express()
-app.use('/api',router())
-createRouter(app)
+app.use(checkApi)
 
-const listener = app.listen(process.env.PORT||3000,()=>{
+
+app.use('/api', router())
+// createRouter(app)
+
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`app run on port ${process.env.PORT}`)
 })
